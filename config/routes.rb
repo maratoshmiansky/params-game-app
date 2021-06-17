ea3950579b7038a871f8a681e2f4e749daed8211
Marat Oshmiansky <maratical@gmail.com>
2021-06-17 11:58:49 -0700

    updates routes again

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "query_params" => "params#query_params"
   get "/name" => "params#name"
-  get "/guess" => "params#guess"
+  get "/guess/:user_guess" => "params#guess"
   get "/segment_params/:this_is_a_key" => "params#segment_params"
 end
