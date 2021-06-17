902ae4054e318ea37d9a6a3684860bad81cc0076
Marat Oshmiansky <maratical@gmail.com>
2021-06-17 11:17:28 -0700

    create routes and actions

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,6 +1,27 @@
 class ParamsController < ApplicationController
   def query_params
-    input_value = params["my_name"]
-    render json: { message: "My name is #{input_value.upcase}" }
+    input_value = params["my_message"]
+    render json: { my_message: "The message is #{input_value}" }
+  end
+
+  def name
+    input_name = params["query_name"]
+    output_message = input_name.upcase
+    if input_name.upcase[0] == "A"
+      output_message = "Hey!  Your name starts with the first letter of the alphabet!"
+    end
+    render json: { message: output_message }
+  end
+
+  def guess
+    winning_number = 32
+    input_guess = params["user_guess"].to_i
+    if input_guess > winning_number
+      output_message = "Pick lower!"
+    elsif input_guess < winning_number
+      output_message = "Pick higher!"
+    else
+      output_message = "You win!!!"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "query_params" => "params#query_params"
+  get "/name" => "params#name"
+  get "/guess" => "params#guess"
 end
